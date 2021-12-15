6c532fcab9b65436fafb4912b5e343dc61f6d934
Mael <mael.thomas@data.gouv.fr>
2021-12-15 16:04:37 +0100

    :sparkles: Clarification de l'utilisation de yjs

--- source/sites/publicodes/conference/ConferenceBar.tsx
+++ source/sites/publicodes/conference/ConferenceBar.tsx
@@ -14,14 +14,9 @@ import { computeHumanMean } from './Stats'
 import { filterExtremes } from './utils'
 import { backgroundConferenceAnimation } from './conferenceStyle'
 import { WebsocketProvider } from 'y-websocket'
+import useYjs from './useYjs'
 
 export default () => {
-	const conference = useSelector((state) => state.conference)
-
-	const { room, ydoc, provider } = conference
-
-	const [users, setUsers] = useState([])
-
 	const situation = useSelector(situationSelector),
 		engine = useEngine(),
 		evaluation = engine.evaluate('bilan'),
@@ -29,58 +24,30 @@ export default () => {
 	const rules = useSelector((state) => state.rules)
 
 	const progress = useSimulationProgress()
+	const { rawElements, users, username, conference } = useYjs()
 
 	const byCategory = extractCategories(rules, engine)
 
 	const nodeValue = correctValue({ nodeValue: rawNodeValue, unit })
-	const [username, setUsername] = usePersistingState('pseudo')
 
-	const [rawElements, setElements] = useState([])
 	const elements = filterExtremes(rawElements)
-	const dispatch = useDispatch()
-
-	const simulations = ydoc.get('simulations', Y.Map)
-
-	const awareness = provider.awareness
 
 	useEffect(() => {
-		console.log('useeffect with ?', conference)
-		if (!conference) {
-			const ydoc = new Y.Doc()
-			const provider = new WebsocketProvider(
-				'ws://ngc.cleverapps.io:8080',
-				room,
-				ydoc
-			)
-			dispatch({ type: 'SET_CONFERENCE', room, ydoc, provider })
-		} else {
-			awareness.on('change', (changes) => {
-				// Whenever somebody updates their awareness information,
-				// we log all awareness information from all users.
-				setUsers(Array.from(awareness.getStates().values()))
-			})
-			simulations.observe((event) => {
-				setElements(simulations.toJSON())
-				console.log('SIMULATIONS', simulations.toJSON())
-			})
-		}
-	}, [room, conference])
+		if (!conference?.ydoc) return null
 
-	useEffect(() => {
-		if (!conference) return null
-
-		const simulations = ydoc.get('simulations', Y.Map)
+		const simulations = conference.ydoc.get('simulations', Y.Map)
 
 		simulations.set(username, { bilan: nodeValue, progress, byCategory })
 	}, [situation])
 
-	if (!conference) return <Link to="/conférence">Lancer une conférence</Link>
+	if (!conference?.ydoc)
+		return <Link to="/conférence">Lancer une conférence</Link>
 
 	const simulationArray = elements && Object.values(elements),
 		result = computeHumanMean(simulationArray.map((el) => el.bilan))
 
 	return (
-		<Link to={'/conférence/' + room} css="text-decoration: none;">
+		<Link to={'/conférence/' + conference.room} css="text-decoration: none;">
 			<div
 				css={`
 					${backgroundConferenceAnimation}
@@ -106,7 +73,9 @@ export default () => {
 					}
 				`}
 			>
-				<span css="text-transform: uppercase">«&nbsp;{room}&nbsp;»</span>
+				<span css="text-transform: uppercase">
+					«&nbsp;{conference.room}&nbsp;»
+				</span>
 				<span>
 					{emoji('🧮')} {result}
 				</span>
