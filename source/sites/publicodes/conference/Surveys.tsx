7e6def7b7795f94081d390d2d8e26749e1f6e8eb
Mael <mael.thomas@data.gouv.fr>
2022-01-26 12:25:34 +0100

    1ère écriture dans Parse. La lib react ne marche pas
    https://www.back4app.com/docs/platform/parse-server-live-query-example

--- source/sites/publicodes/conference/Surveys.tsx
+++ source/sites/publicodes/conference/Surveys.tsx
@@ -1,14 +1,37 @@
 import { useParseQuery } from '@parse/react'
 import Parse from 'parse'
+import { useEffect } from 'react'
+import useDatabase from './useDatabase'
+
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY) //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = 'https://parseapi.back4app.com/'
 
 export default () => {
 	const parseQuery = new Parse.Query('Survey')
 
-	const { isLive, isLoading, isSyncing, results, count, error, reload } =
-		useParseQuery(parseQuery, {
+	const database = useDatabase()
+
+	/*
+	const queryResult = useParseQuery(parseQuery, {
 			enableLocalDatastore: true, // Enables cache in local datastore (default: true)
 			enableLiveQuery: true, // Enables live query for real-time update (default: true)
+		}),
+		{ isLive, isLoading, isSyncing, results, count, error, reload } =
+			queryResult
+
+	console.log(queryResult)
+	*/
+
+	const results = []
+
+	useEffect(() => {
+		const Survey = Parse.Object.extend('Survey')
+		const newPost = new Survey()
+		newPost.save({
+			name: 'monsondage' + Math.round(Math.random() * 10),
+			answers: ['a', 'b'],
 		})
+	}, [])
 
 	return results ? (
 		results.map((result, index) => (
--- source/sites/publicodes/conference/useDatabase.tsx
+++ source/sites/publicodes/conference/useDatabase.tsx
@@ -1,13 +1,14 @@
 import { useMemo } from 'react'
 import { initializeParse } from '@parse/react'
+import Parse from 'parse'
 
 export default () => {
 	const database = useMemo(
 		() =>
 			initializeParse(
-				'PARSE_SERVER_URL',
-				'PARSE_APPLICATION_ID',
-				'PARSE_JAVASCRIPT_KEY'
+				PARSE_SERVER_URL,
+				PARSE_APPLICATION_ID,
+				PARSE_JAVASCRIPT_KEY
 			),
 		[]
 	)
