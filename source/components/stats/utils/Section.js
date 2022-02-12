e58863649ee639447aff1f3e87e0fc36f41c4527
Clement AUGER <clem.auger@hotmail.fr>
2022-02-12 12:54:53 +0100

    Ajout police Marianne
    Je trouve que c'est plus esthétique pour la page stats

--- source/components/stats/utils/Section.js
+++ source/components/stats/utils/Section.js
@@ -2,11 +2,13 @@ import styled from 'styled-components'
 
 const Section = styled.section`
 	margin: 0 auto 5.5rem;
+	font-family: 'Marianne', sans-serif;
 `
 Section.TopTitle = styled.h1`
 	font-size: 5rem;
 	text-align: center;
 	margin-bottom: 3rem;
+	font-family: 'Marianne', sans-serif;
 `
 Section.Title = styled.h2`
 	font-size: 2.5em;
