45355e6b99f18706fd8fc6db3eef85d46479a200
Mael <mael.thomas@data.gouv.fr>
2022-03-16 14:39:11 +0100

    Changement d'API de localisation d'IP

--- source/components/useLocalisation.ts
+++ source/components/useLocalisation.ts
@@ -1,9 +1,16 @@
 import { useEffect } from 'react'
 
+const API =
+	'https://api.ipgeolocation.io/ipgeo?apiKey=a012a48f6d0244ed967df27ce20415ab'
+
+// Other alternatives :
+// https://positionstack.com/product
+// https://www.abstractapi.com/ip-geolocation-api?fpr=geekflare#pricing
+
 export default () => {
 	useEffect(() => {
 		return async () => {
-			const req = await fetch(`https://ip-api.com/json`),
+			const req = await fetch(API),
 				data = await req.json()
 			console.log(data)
 		}
