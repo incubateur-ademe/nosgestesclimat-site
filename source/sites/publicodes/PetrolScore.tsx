572b0a73fa9399ec19a724973f259338384b3487
Clement AUGER <clem.auger@hotmail.fr>
2022-03-23 19:51:56 +0100

    Evaluation de la variable pétrole

--- source/sites/publicodes/PetrolScore.tsx
+++ source/sites/publicodes/PetrolScore.tsx
@@ -1,19 +1,25 @@
-export default () => (
-	<div css="display: flex; align-items: center">
-		<img
-			src="/images/pompe-essence.svg"
-			css="width: 3rem; margin-right: .4rem"
-			alt="Icône représentant une pompe à pétrole"
-		/>
-		<div
-			css="display: flex; flex-direction: column"
-			title="200 litres de pétrole"
-		>
-			<div css="font-size: 120%; font-weight: bold">
-				{Math.round(Math.random() * 20 + 10)} pleins
-			</div>
+import { useEngine } from 'Components/utils/EngineContext'
+import { correctValue } from 'Components/publicodesUtils'
 
-			<div css="@media (max-width: 800px){display: none}">de pétrole</div>
+export default () => {
+	const engine = useEngine()
+	const evaluation = engine.evaluate('pétrole')
+	const nbrePleins = Math.round(correctValue(evaluation))
+	return (
+		<div css="display: flex; align-items: center">
+			<img
+				src="/images/pompe-essence.svg"
+				css="width: 3rem; margin-right: .4rem"
+				alt="Icône représentant une pompe à pétrole"
+			/>
+			<div
+				css="display: flex; flex-direction: column"
+				title="200 litres de pétrole"
+			>
+				<div css="font-size: 120%; font-weight: bold">{nbrePleins} pleins</div>
+
+				<div css="@media (max-width: 800px){display: none}">de pétrole</div>
+			</div>
 		</div>
-	</div>
-)
+	)
+}
