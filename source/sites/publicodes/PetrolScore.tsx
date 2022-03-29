d65fd06db3ca249e89a01d44c09c18b2a12bbc66
Clement AUGER <clem.auger@hotmail.fr>
2022-03-29 11:22:45 +0200

    TEMP comparaison 2 méthodes de calcul

--- source/sites/publicodes/PetrolScore.tsx
+++ source/sites/publicodes/PetrolScore.tsx
@@ -3,8 +3,10 @@ import { correctValue } from 'Components/publicodesUtils'
 
 export default () => {
 	const engine = useEngine()
-	const evaluation = engine.evaluate('pétrole . pleins')
-	const nbrePleins = Math.round(correctValue(evaluation))
+	const evaluation_EROI = engine.evaluate('pétrole . pleins EROI')
+	const nbrePleins_EROI = Math.round(correctValue(evaluation_EROI))
+	const evaluation_energ = engine.evaluate('pétrole . pleins énergie')
+	const nbrePleins_energ = Math.round(correctValue(evaluation_energ))
 	return (
 		<div css="display: flex; align-items: center">
 			<img
@@ -16,7 +18,12 @@ export default () => {
 				css="display: flex; flex-direction: column"
 				title="200 litres de pétrole"
 			>
-				<div css="font-size: 120%; font-weight: bold">{nbrePleins} pleins</div>
+				<div css="font-size: 120%; font-weight: bold">
+					{nbrePleins_EROI} pleins (EROI)
+				</div>
+				<div css="font-size: 120%; font-weight: bold">
+					{nbrePleins_energ} pleins (Energie)
+				</div>
 
 				<div css="@media (max-width: 800px){display: none}">de pétrole</div>
 			</div>
