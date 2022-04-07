3239349ff426947232c3e5ddee01c9f68afeb1f0
Mael <mael.thomas@data.gouv.fr>
2022-04-07 14:42:14 +0200

    :sparkles: Déplacementde l'icône de partage dans nouveau fichier

--- source/components/ShareButton.tsx
+++ source/components/ShareButton.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 import emoji from './emoji'
+import ShareButtonIcon from './ShareButtonIcon'
 
 export default ({ text, url, title, color, label }) =>
 	navigator.share ? (
@@ -13,7 +14,7 @@ export default ({ text, url, title, color, label }) =>
 					.catch((error) => console.log('Error sharing', error))
 			}
 		>
-			<Icon />
+			<ShareButtonIcon />
 			{label && <span>{label}</span>}
 			{/* Created by Barracuda from the Noun Project */}
 		</Button>
@@ -49,7 +50,7 @@ export const DesktopShareButton = ({ label, text, color }) => {
 				)
 			}}
 		>
-			<Icon />
+			<ShareButtonIcon />
 			{!copySuccess ? (
 				label ? (
 					<span>{label}</span>
@@ -63,53 +64,6 @@ export const DesktopShareButton = ({ label, text, color }) => {
 		</Button>
 	)
 }
-
-const Icon = ({}) => (
-	<div
-		css={`
-			background: var(--color);
-			width: 3rem;
-			height: 3rem;
-			border-radius: 2rem;
-			padding: 0.5rem;
-			margin: 0.6rem;
-			svg {
-				width: 2rem;
-			}
-		`}
-	>
-		<svg
-			aria-hidden="true"
-			version="1.1"
-			x="0px"
-			y="0px"
-			viewBox="0 0 100 100"
-			width="4rem"
-		>
-			<g transform="translate(0,-952.36218)">
-				<path
-					css={`
-						text-indent: 0;
-						text-transform: none;
-						direction: ltr;
-						block-progression: tb;
-						baseline-shift: baseline;
-						color: var(--textColor);
-						enable-background: accumulate;
-					`}
-					d="m 67,971.36217 c -5.4991,0 -10,4.50082 -10,10 0,1.123 0.1857,2.20885 0.5312,3.21875 l -17.125,11.125 c -1.8339,-2.03453 -4.4689,-3.34375 -7.4062,-3.34375 -5.4991,0 -10,4.50082 -10,10.00003 0,5.4992 4.5009,10 10,10 2.9494,0 5.6028,-1.2946 7.4375,-3.3437 l 17.0937,11.1249 c -0.3515,1.0177 -0.5312,2.0857 -0.5312,3.2188 0,5.4992 4.5009,10 10,10 5.4991,0 10,-4.5008 10,-10 0,-5.4992 -4.5009,-10 -10,-10 -2.9373,0 -5.5723,1.3092 -7.4062,3.3438 l -17.125,-11.125 c 0.3455,-1.0099 0.5312,-2.0958 0.5312,-3.2188 0,-1.1331 -0.1797,-2.2011 -0.5312,-3.21873 l 17.0937,-11.12505 c 1.8347,2.04912 4.4881,3.34375 7.4375,3.34375 5.4991,0 10,-4.50082 10,-10 0,-5.49918 -4.5009,-10 -10,-10 z m 0,4 c 3.3373,0 6,2.66262 6,6 0,3.33738 -2.6627,6 -6,6 -3.3373,0 -6,-2.66262 -6,-6 0,-3.33738 2.6627,-6 6,-6 z m -34,21 c 3.3373,0 6,2.6626 6,6.00003 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.33743 2.6627,-6.00003 6,-6.00003 z m 34,21.00003 c 3.3373,0 6,2.6626 6,6 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.3374 2.6627,-6 6,-6 z"
-					fill="var(--textColor)"
-					fillOpacity="1"
-					stroke="none"
-					marker="none"
-					visibility="visible"
-					display="inline"
-					overflow="visible"
-				/>
-			</g>
-		</svg>
-	</div>
-)
 
 const Button = styled.button`
 	margin: 0 auto;
