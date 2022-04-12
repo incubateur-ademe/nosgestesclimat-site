970a64eb76290331b1f3e9bf6e79a8764002d2b2
Mael <mael.thomas@data.gouv.fr>
2022-04-12 17:10:54 +0200

    Amélioration des blocs de dialogue du mode groupe
    Utilisation d'un composant partagé; saut à la ligne des boutons si petit
    écran

--- source/sites/publicodes/conference/NoTestMessage.tsx
+++ source/sites/publicodes/conference/NoTestMessage.tsx
@@ -1,62 +1,42 @@
 import emoji from 'react-easy-emoji'
 import { Link } from 'react-router-dom'
+import IllustratedMessage from '../../../components/ui/IllustratedMessage'
 
 export default ({ setHasDataState }) => (
-	<div
-		className="ui__ card"
-		css={`
-			max-width: 30rem;
-			margin: 0 auto;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-		`}
-	>
-		<span
-			css={`
-				img {
-					font-size: 300%;
-					margin: 0.6rem !important;
-				}
-			`}
-		>
-			{emoji('🕳️')}
-		</span>
-		<div
-			css={`
-				margin: 0 0 1rem 0;
-			`}
-		>
-			<p>
-				Bienvenue dans le mode groupe de Nos Gestes Climat ! Vous n'avez pas
-				encore débuté votre test, lancez-vous !
-			</p>{' '}
-		</div>
-		<div
-			css={`
-				display: flex;
-				flex-direction: row;
-				align-items: space-evenly;
-				margin-bottom: 1rem;
-				> button {
-					width: 50% !important;
-					margin: 0.1rem;
-				}
-			`}
-		>
-			<button>
-				<Link className="ui__ button plain" to={'/simulateur/bilan'}>
-					Faire mon test
-				</Link>
-			</button>
-			<button
-				className="ui__ small button"
-				onClick={() => {
-					setHasDataState(true)
-				}}
-			>
-				Accéder directement aux résultats du groupe{' '}
-			</button>
-		</div>
-	</div>
+	<IllustratedMessage
+		emoji="🏁"
+		message={
+			<div>
+				<p>
+					Bienvenue dans le mode groupe de Nos Gestes Climat ! Vous n'avez pas
+					encore débuté votre test, lancez-vous !
+				</p>
+				<div
+					css={`
+						display: flex;
+						flex-wrap: wrap;
+						justify-content: space-evenly;
+						margin: 1rem 0 0;
+						button {
+							margin: 0.4rem 0;
+						}
+					`}
+				>
+					<button>
+						<Link className="ui__ button plain" to={'/simulateur/bilan'}>
+							Faire mon test
+						</Link>
+					</button>
+					<button
+						className="ui__ small button"
+						onClick={() => {
+							setHasDataState(true)
+						}}
+					>
+						Accéder directement aux résultats du groupe{' '}
+					</button>
+				</div>
+			</div>
+		}
+	/>
 )
