05b3fd0f06a9cae1543304caf21f51cb3e6a1ca4
Mael <mael.thomas@data.gouv.fr>
2022-04-12 10:16:58 +0200

    Style plus discret et sophistique pour le bloc FAQ

--- source/sites/publicodes/BandeauContribuer.js
+++ source/sites/publicodes/BandeauContribuer.js
@@ -4,9 +4,22 @@ import { Link } from 'react-router-dom'
 
 export default () => {
 	return (
-
-		<div css=" display: flex; flex-wrap: wrap; justify-content: center;text-align: center; color: black; margin: 2rem 0">
-			<span>Une question, un problème ? {emoji('📮')} </span>
+		<div
+			css={`
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: center;
+				text-align: center;
+				color: black;
+				margin: 2rem auto;
+				max-width: 16rem;
+				line-height: 1.3rem;
+				background: var(--lightestColor);
+				border-radius: 1rem;
+				padding: 0.4rem;
+			`}
+		>
+			<span>Une question, un problème ? </span>
 			<Link to={'/contribuer?fromLocation=' + window.location}>
 				Découvrez la FAQ !
 			</Link>
