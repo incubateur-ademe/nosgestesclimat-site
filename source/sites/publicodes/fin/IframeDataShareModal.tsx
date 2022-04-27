03c98690dfa70c0aa2ecc5e44df9ed00df991ae1
Clem-mrtn <76995768+Clem-mrtn@users.noreply.github.com>
2022-04-27 15:40:20 +0200

    Update IframeDataShareModal.tsx

--- source/sites/publicodes/fin/IframeDataShareModal.tsx
+++ source/sites/publicodes/fin/IframeDataShareModal.tsx
@@ -44,8 +44,8 @@ export default ({ data }) => {
 	const text = (
 		<div>
 			<p>
-				En cliquant sur le bouton Accepter, vous autorisez {parent} à récupérer le
-				résumé de votre test d'empreinte climat.
+				En cliquant sur le bouton Accepter, vous autorisez {parent} à récupérer le 
+	      bilan de votre empreinte climat.
 			</p>
 			<p>
 				Il s'agit de vos résultats sur les grandes catégories (transport,
