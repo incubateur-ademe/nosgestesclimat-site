cf3cc9af4c349e1d3114d52dd3478926cfc45000
Clement AUGER <clem.auger@hotmail.fr>
2022-05-19 18:25:50 +0200

    Amélioration composant emoji pour accessibilté

--- source/components/emoji.js
+++ source/components/emoji.js
@@ -4,16 +4,23 @@ import twemoji from 'twemoji'
 // This file looks like a gaz factory, as we say in french. Feel free to rewrite it
 
 // Here text should be limited to emojis. Nothing else
-export const getEmojiComponents = (text) => {
+
+export const getEmojiComponents = (text, title) => {
+	const attributesCallback = () => {
+		return {
+			'aria-label': title ? title : '',
+			'aria-hidden': title ? 'false' : 'true',
+		}
+	}
 	const parts = twemoji
 		.parse(text, {
+			attributes: attributesCallback,
 			folder: 'svg',
 			ext: '.svg',
 		})
 		.split('<img')
 		.map((el) => el && '<img' + el)
 		.filter(Boolean)
-
 	return parts.map((part) => (
 		<EmojiStyle
 			dangerouslySetInnerHTML={{
@@ -24,11 +31,20 @@ export const getEmojiComponents = (text) => {
 }
 
 // Here text can include emojis and text
-export const Twemoji = ({ text }) => {
+export const Twemoji = ({ text, label }) => {
+	const attributesCallback = () => {
+		return {
+			'aria-label': label ? label : '',
+			'aria-hidden': label ? 'false' : 'true',
+		}
+	}
+
 	const parsed = twemoji.parse(text, {
+		attributes: attributesCallback,
 		folder: 'svg',
 		ext: '.svg',
 	})
+
 	return parsed.includes('<img class="emoji"') ? (
 		<EmojiStyle
 			dangerouslySetInnerHTML={{
@@ -40,8 +56,8 @@ export const Twemoji = ({ text }) => {
 	)
 }
 
-export default (text) => {
-	return <Twemoji text={text} />
+export default (text, label) => {
+	return <Twemoji text={text} label={label} />
 }
 
 const EmojiStyle = styled.span`
