8e7a72d786d7df6d9d8abacc443eeb3b92a5c547
Mael <mael.thomas@data.gouv.fr>
2022-06-08 17:55:14 +0200

    MAJ sitemap, surtout avec de nouvelles URL du parcours

--- source/sites/publicodes/base-URLs.js
+++ source/sites/publicodes/base-URLs.js
@@ -2,4 +2,11 @@ module.exports = `https://nosgestesclimat.fr/simulateur/bilan
 https://nosgestesclimat.fr/vie-privée
 https://nosgestesclimat.fr/documentation
 https://nosgestesclimat.fr/à-propos
+https://nosgestesclimat.fr/nouveautés
+https://nosgestesclimat.fr/groupe
+https://nosgestesclimat.fr/profil
+https://nosgestesclimat.fr/partenaires
+https://nosgestesclimat.fr/contribuer
+https://nosgestesclimat.fr/personas
+https://nosgestesclimat.fr/stats
 `
