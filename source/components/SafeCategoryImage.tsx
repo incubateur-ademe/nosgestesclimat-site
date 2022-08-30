52476f986e31591c1f8985f4c1d5e1b003c67c8b
Mael <mael.thomas@data.gouv.fr>
2022-08-30 18:38:14 +0200

    Icônes nouvelle gen sur page profil

--- source/components/SafeCategoryImage.tsx
+++ source/components/SafeCategoryImage.tsx
@@ -1,14 +1,26 @@
-export default ({ element }) => (
-	<img
-		css={`
-			filter: grayscale(1) invert(1) brightness(1.8);
-			width: 2.5rem;
-			height: auto;
-		`}
-		src={`/images/model/${element.dottedName}.svg`}
-		onError={({ currentTarget }) => {
-			currentTarget.onerror = null
-			currentTarget.src = '/images/three-dots.svg'
-		}}
-	/>
-)
+import { useState } from 'react'
+import emoji from './emoji'
+
+export default ({ element, whiteBackground = false }) => {
+	const [fail, setFail] = useState(false)
+	return !fail ? (
+		<img
+			css={`
+				${!whiteBackground &&
+				`
+				filter: grayscale(1) invert(1) brightness(1.8);
+				`}
+				width: 2.5rem;
+
+				height: auto;
+			`}
+			src={`/images/model/${element.dottedName}.svg`}
+			onError={({ currentTarget }) => {
+				currentTarget.onerror = null
+				setFail(true)
+			}}
+		/>
+	) : (
+		emoji(element.rawNode?.icônes || element.icons || '')
+	)
+}
