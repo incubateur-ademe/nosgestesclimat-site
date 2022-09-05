08e96718714dc382b780c57b919eb171835f51cb
Mael <mael.thomas@data.gouv.fr>
2022-09-05 18:05:58 +0200

    Ne pas charger les règles plusieurs fois dans la navigation

--- source/RulesProvider.js
+++ source/RulesProvider.js
@@ -12,6 +12,7 @@ import useBranchData from 'Components/useBranchData'
 import Engine from 'publicodes'
 import { useEffect, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { usePersistingState } from './components/utils/persistState'
 
 export default ({ children }) => {
 	const branchData = useBranchData()
@@ -20,9 +21,14 @@ export default ({ children }) => {
 	const dispatch = useDispatch()
 
 	const setRules = (rules) => dispatch({ type: 'SET_RULES', rules })
+	// This component is not initialized just once. Hence we need
+	// to share the understanding of the version of the rules stored
+	const [rulesURL, setRulesURL] = usePersistingState('rulesURL', undefined)
 
 	useEffect(() => {
 		if (!branchData.loaded) return
+		console.log('rulesURL', rulesURL)
+		if (rules && branchData.deployURL === rulesURL) return
 		if (NODE_ENV === 'development' && branchData.shouldUseLocalFiles) {
 			// Rules are stored in nested yaml files
 			const req = require.context(
@@ -37,11 +43,13 @@ export default ({ children }) => {
 			}, {})
 
 			setRules(rules)
+			setRulesURL(branchData.deployURL)
 		} else {
 			fetch(branchData.deployURL + '/co2.json', { mode: 'cors' })
 				.then((response) => response.json())
 				.then((json) => {
 					setRules(json)
+					setRulesURL(branchData.deployURL)
 				})
 		}
 	}, [branchData.deployURL, branchData.loaded, branchData.shouldUseLocalFiles])
