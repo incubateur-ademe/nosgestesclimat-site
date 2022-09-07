2ab21d48649111cae259e20cc150ad19bd919535
Mael <mael.thomas@data.gouv.fr>
2022-09-07 15:37:10 +0200

    Chargement de la page nouveautés en lazy

--- source/sites/publicodes/App.tsx
+++ source/sites/publicodes/App.tsx
@@ -1,7 +1,6 @@
 import Route404 from 'Components/Route404'
 import { sessionBarMargin } from 'Components/SessionBar'
 import 'Components/ui/index.css'
-import News from 'Pages/News'
 import React, { Suspense, useContext, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Redirect, useLocation } from 'react-router'
@@ -45,6 +44,7 @@ const GuideGroupeLazy = React.lazy(() => import('./pages/GuideGroupe'))
 const DocumentationContexteLazy = React.lazy(
 	() => import('./pages/DocumentationContexte')
 )
+const News = React.lazy(() => import('Pages/News'))
 
 let tracker = devTracker
 if (NODE_ENV === 'production') {
@@ -235,7 +235,11 @@ const Router = ({}) => {
 			/>
 			<Route
 				path={`${encodeURIComponent('nouveautés')}/*`}
-				element={<News />}
+				element={
+					<Suspense fallback={<Loading />}>
+						<News />
+					</Suspense>
+				}
 			/>
 			<Route
 				path="/guide"
@@ -297,7 +301,7 @@ const Router = ({}) => {
 				path="/tutoriel"
 				element={
 					<Suspense fallback={<Loading />}>
-							<TutorialLazy />
+						<TutorialLazy />
 					</Suspense>
 				}
 			/>
