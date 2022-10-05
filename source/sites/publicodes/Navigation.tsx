ab3670d8dd5519a6219c79f9b8641e53cc128866
Mael <mael.thomas@data.gouv.fr>
2022-10-05 18:49:00 +0200

    Nouveau système d'agencement fluide /nouveautés et /documentation

--- source/sites/publicodes/Navigation.tsx
+++ source/sites/publicodes/Navigation.tsx
@@ -4,51 +4,51 @@ import { Link } from 'react-router-dom'
 import Logo from '../../components/Logo'
 import { IframeOptionsContext } from '../../components/utils/IframeOptionsProvider'
 import useMediaQuery from '../../components/utils/useMediaQuery'
+import { isFluidLayout } from './App'
 import SkipLinks from './SkipLinks'
 
-export default ({ isHomePage }) => {
+export default ({ fluidLayout }) => {
 	const pathname = decodeURIComponent(location.pathname)
 
 	const { isIframe } = useContext(IframeOptionsContext)
 
 	const largeScreen = useMediaQuery('(min-width: 800px)')
 
-	const hideSessionBar =
-		pathname === '/' ||
-		pathname.startsWith('/nouveautés') ||
-		pathname.startsWith('/documentation')
-
 	return (
 		<>
 			<SkipLinks />
-			<nav
-				id="mainNavigation"
-				tabIndex="0"
-				css={`
-					display: flex;
-					justify-content: center;
-					margin: 0.6rem auto;
+			{!fluidLayout && (
+				<nav
+					id="mainNavigation"
+					tabIndex="0"
+					css={`
+						display: flex;
+						justify-content: center;
+						margin: 0.6rem auto;
 
-					outline: none !important;
+						outline: none !important;
 
-					@media (min-width: 800px) {
-						flex-shrink: 0;
-						width: 12rem;
-						height: 100vh;
-						${isIframe && `height: 100% !important;`}
-						overflow: auto;
-						position: sticky;
-						top: 0;
-						flex-direction: column;
-						justify-content: start;
-						border-right: 1px solid #eee;
-					}
-					${isHomePage && `display: none`}
-				`}
-			>
-				<Logo showText={largeScreen} size={largeScreen ? 'medium' : 'small'} />
-				{!hideSessionBar && <SessionBar />}
-			</nav>
+						@media (min-width: 800px) {
+							flex-shrink: 0;
+							width: 12rem;
+							height: 100vh;
+							${isIframe && `height: 100% !important;`}
+							overflow: auto;
+							position: sticky;
+							top: 0;
+							flex-direction: column;
+							justify-content: start;
+							border-right: 1px solid #eee;
+						}
+					`}
+				>
+					<Logo
+						showText={largeScreen}
+						size={largeScreen ? 'medium' : 'small'}
+					/>
+					<SessionBar />
+				</nav>
+			)}
 		</>
 	)
 }
