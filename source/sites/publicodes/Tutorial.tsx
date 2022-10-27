0dc39f04211d571521ca5b321dfd5c7e078908e5
Mael <mael.thomas@data.gouv.fr>
2022-10-27 11:46:33 +0200

    :bug: La slide 7 était notée 6 dans i18n

--- source/sites/publicodes/Tutorial.tsx
+++ source/sites/publicodes/Tutorial.tsx
@@ -338,19 +338,19 @@ const slides = [
 	),
 	() => (
 		<>
-			<Trans i18nKey={`publicodes.Tutorial.slide6`}>
+			<Trans i18nKey={`publicodes.Tutorial.slide7`}>
 				<h1>Alors, c'est parti ?</h1>
-				<p>Quelques astuces pour vous aider à compléter le test.</p>
-				<blockquote>
+				<p>Quelques astuces pour vous aider à compléter le test :</p>
+				<blockquote key="individuel">
 					👤 Répondez aux questions en votre nom, pas au nom de votre foyer :
 					c'est un test individuel.
 				</blockquote>
-				<blockquote>
+				<blockquote key="perso">
 					💼 Répondez pour votre vie perso, pas pour votre boulot ou études.{' '}
 					<em>Une seule exception </em>: votre trajet domicile-travail doit être
 					inclus dans les km parcourus.
 				</blockquote>
-				<blockquote>
+				<blockquote key="questions">
 					❓️ D'autres questions ? Consultez notre{' '}
 					<Link to="/contribuer">FAQ</Link> à tout moment.
 				</blockquote>
