5c8b4020e69a3c3915bbce894354a52712871003
Mael <mael.thomas@data.gouv.fr>
2022-11-10 11:39:17 +0100

    Animation de l'explication du score

--- source/components/ScoreExplanation.tsx
+++ source/components/ScoreExplanation.tsx
@@ -1,10 +1,15 @@
+import { motion } from 'framer-motion'
 import { Trans } from 'react-i18next'
 import DefaultFootprint from '../sites/publicodes/DefaultFootprint'
 
 export default ({ openExplanation, setOpenExplanation }) => {
 	return (
 		openExplanation && (
-			<div
+			<motion.div
+				positionTransition
+				initial={{ opacity: 0, y: 50, scale: 0.3 }}
+				animate={{ opacity: 1, y: 0, scale: 1 }}
+				exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
 				className="ui__ card"
 				css={`
 					position: relative;
@@ -79,7 +84,7 @@ export default ({ openExplanation, setOpenExplanation }) => {
 						<Trans>J'ai compris</Trans>
 					</button>
 				</div>
-			</div>
+			</motion.div>
 		)
 	)
 }
