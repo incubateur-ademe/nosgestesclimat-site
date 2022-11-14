4b47785e25351d859a1fd1d9f301618d5eb41a2a
Clement AUGER <clem.auger@hotmail.fr>
2022-11-14 11:25:01 +0100

    we use answeredquestion instead of situation

--- source/sites/publicodes/ScoreBar.tsx
+++ source/sites/publicodes/ScoreBar.tsx
@@ -9,9 +9,11 @@ import { buildEndURL } from '../../components/SessionBar'
 import { lightenColor } from '../../components/utils/colors'
 import { TrackerContext } from '../../components/utils/withTracker'
 import {
+	answeredQuestionsSelector,
 	objectifsSelector,
 	situationSelector,
 } from '../../selectors/simulationSelectors'
+
 import HumanWeight, { DiffHumanWeight } from './HumanWeight'
 import PetrolScore from './PetrolScore'
 
@@ -43,9 +45,10 @@ export default ({ actionMode = false, demoMode = false }) => {
 	const tracker = useContext(TrackerContext)
 	const tutorials = useSelector((state) => state.tutorials)
 
-	const situationLength = Object.keys(situation).length
+	const answeredQuestions = useSelector(answeredQuestionsSelector)
+	const answeredQuestionsLength = answeredQuestions.length
 
-	const blur = situationLength === 0
+	const blur = answeredQuestionsLength === 0
 
 	useEffect(() => {
 		if (blur && !tutorials['scoreExplanation']) {
@@ -168,7 +171,7 @@ export default ({ actionMode = false, demoMode = false }) => {
 				<ScoreExplanation
 					openExplanation={openExplanation}
 					setOpenExplanation={setOpenExplanation}
-					situationLength={situationLength}
+					answeredQuestionsLength={answeredQuestionsLength}
 				/>
 			</div>
 		</div>
