a585a0daec232f197d544ea76c82ca5ae8e686a5
Clement AUGER <clem.auger@hotmail.fr>
2022-11-15 12:09:03 +0100

    wording bouton retour page de fin

--- source/sites/publicodes/fin/index.tsx
+++ source/sites/publicodes/fin/index.tsx
@@ -96,12 +96,12 @@ export default ({}) => {
 				) : nextQuestions.length > 1 ? (
 					<button class="ui__ button plain">
 						{' '}
-						← <Trans>Terminer ma simulation</Trans>
+						← <Trans>Revenir aux questions</Trans>
 					</button>
 				) : (
 					<button className="ui__ simple small push-left button">
 						{' '}
-						← <Trans>Revenir à ma simulation</Trans>
+						← <Trans>Revenir au test</Trans>
 					</button>
 				)}
 			</Link>
