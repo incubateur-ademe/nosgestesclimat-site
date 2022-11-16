7a44e816d3488fc41f104fba056a470f5d0f91cf
Emile Rolley <emile.rolley@tuta.io>
2022-11-16 14:48:01 +0100

    fix(t9n): use unique i18nKey

--- source/sites/publicodes/TranslationContribution.tsx
+++ source/sites/publicodes/TranslationContribution.tsx
@@ -112,7 +112,7 @@ export default ({}) => {
 					</form>
 				) : (
 					<p role="status">
-						<Trans i18nKey={'publicodes.Contribution.remerciements'}>
+						<Trans i18nKey={'publicodes.TranslationContribution.remerciements'}>
 							Merci 😍! Suivez l'avancement de votre suggestion en cliquant sur{' '}
 							<a href={URL}>ce lien</a>.
 						</Trans>
