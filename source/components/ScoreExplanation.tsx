b0801c84a8cdc12e266c9fdd646dd975b8839502
Mael <mael.thomas@data.gouv.fr>
2022-11-23 09:44:39 +0100

    Plus besoin de drop-shadow importante, il n'y a plus rien en-dessous

--- source/components/ScoreExplanation.tsx
+++ source/components/ScoreExplanation.tsx
@@ -25,7 +25,7 @@ export default ({ openExplanation, setOpenExplanation, situationLength }) => {
 						bottom: 18rem;
 						margin-left: 0.5rem;
 						margin-right: 0.5rem;
-						filter: drop-shadow(0rem -1rem 2rem var(--darkColor));
+						filter: drop-shadow(0rem 0rem 1rem var(--darkColor));
 					}
 				`}
 			>
