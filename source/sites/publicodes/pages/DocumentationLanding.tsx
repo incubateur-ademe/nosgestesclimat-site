5fcdde37bd93c3effdf0ec3e7b8241ff4c68720c
Mael <mael.thomas@data.gouv.fr>
2022-11-25 17:14:14 +0100

    1er design pour l'édito de /documentation

--- source/sites/publicodes/pages/DocumentationLanding.tsx
+++ source/sites/publicodes/pages/DocumentationLanding.tsx
@@ -1,5 +1,10 @@
+import Markdown from 'markdown-to-jsx'
+import { utils } from 'publicodes'
 import { Trans, useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { splitName } from '../../../components/publicodesUtils'
+import SafeCategoryImage from '../../../components/SafeCategoryImage'
 import SearchBar from '../../../components/SearchBar'
 import Meta from '../../../components/utils/Meta'
 import editorialisedModels from './editorialisedModels.yaml'
@@ -8,9 +13,11 @@ export default function DocumentationLanding() {
 	const { t } = useTranslation()
 	const rules = useSelector((state) => state.rules)
 	const editos = Object.entries(editorialisedModels).map(([key, value]) => ({
+		dottedName: key,
 		...rules[key],
 		edito: value.label,
 	}))
+	const getColor = (dottedName) => rules[splitName(dottedName)[0]].couleur
 	return (
 		<div className="ui__ container">
 			<Meta
@@ -36,10 +43,62 @@ export default function DocumentationLanding() {
 					}
 				`}
 			>
-				{editos.map(({ icônes, color, dottedName, title, edito }) => (
-					<li key={dottedName} className="ui__ card box">
-						{icônes}
-						{edito}
+				{editos.map(({ icônes, color, dottedName, title, edito, couleur }) => (
+					<li
+						key={dottedName}
+						className="ui__ card box"
+						css={`
+							background: ${getColor(dottedName) || 'var(--color)'} !important;
+							a {
+								text-decoration: none;
+								z-index: 1;
+								display: flex;
+								align-items: center;
+								height: 100%;
+								h2 {
+									margin-top: 0rem;
+									text-align: center;
+									display: inline;
+									font-size: 120%;
+									line-height: 1.3rem;
+									display: inline-block;
+									color: white;
+									@media (min-width: 800px) {
+										font-size: 110%;
+									}
+									font-weight: 400;
+									strong {
+										font-weight: bold;
+									}
+								}
+								text-decoration: none;
+							}
+							height: 6rem;
+							width: 100%;
+							position: relative;
+						`}
+					>
+						<Link to={'/documentation/' + utils.encodeRuleName(dottedName)}>
+							<span
+								css={`
+									position: absolute;
+									top: 50%;
+									transform: translateX(-50%) translateY(-50%);
+									left: 50%;
+									font-size: 600%;
+									white-space: nowrap;
+									mix-blend-mode: lighten;
+									filter: grayscale(1);
+									opacity: 0.25;
+									img {
+										width: 12rem;
+									}
+								`}
+							>
+								<SafeCategoryImage element={{ dottedName }} />
+							</span>
+							<h2>{<Markdown>{edito}</Markdown>}</h2>
+						</Link>
 					</li>
 				))}
 			</ol>
