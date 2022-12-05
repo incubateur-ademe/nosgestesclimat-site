f1b6237f188bd430d41b504f7c7a4b1590be75d4
Mael <mael.thomas@data.gouv.fr>
2022-12-05 16:38:13 +0100

    Meilleur block modèle intéractif

--- source/sites/publicodes/ModelDemoBlock.tsx
+++ source/sites/publicodes/ModelDemoBlock.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react'
-import { useEngine } from '../../components/utils/EngineContext'
-import { WithEngine } from '../../RulesProvider'
-import InlineCategoryChart from './chart/InlineCategoryChart'
+import Engine from 'publicodes'
+import { useEffect, useMemo, useState } from 'react'
+import { useSelector } from 'react-redux'
 import HumanWeight from './HumanWeight'
 
 const demoDottedNames = [
@@ -10,40 +9,65 @@ const demoDottedNames = [
 	"logement . construction . durée d'amortissement",
 ]
 
+const indicatorsKeys = ['bilan', 'transport . empreinte', 'logement']
 export default () => {
 	const [situation, setSituation] = useState({})
-	const engine = useEngine()
-	const bilan = engine.evaluate('bilan')
+	const rules = useSelector((state) => state.rules)
+	const engine = useMemo(() => {
+		return new Engine(rules)
+	}, [rules])
+
+	const [indicators, setIndicators] = useState(
+		Object.fromEntries(indicatorsKeys.map((el) => [el, null]))
+	)
 
 	useEffect(() => {
 		engine.setSituation(situation)
+		setIndicators(
+			Object.fromEntries(
+				indicatorsKeys.map((indicator) => [
+					indicator,
+					engine.evaluate(indicator).nodeValue,
+				])
+			)
+		)
 	}, [situation])
 
-	return (
-		<WithEngine>
-			<div>
-				<ul>
-					{demoDottedNames.map((el) => (
-						<li key={el}>
-							<label>
-								{el}{' '}
-								<input
-									type="number"
-									onChange={(e) =>
-										e.target.value &&
-										setSituation({ ...situation, [el]: e.target.value })
-									}
-								/>
-							</label>
-						</li>
-					))}
-				</ul>
+	console.log(situation, indicators)
 
-				<div>
-					<HumanWeight nodeValue={bilan.nodeValue} />
-				</div>
-				<InlineCategoryChart />
+	const onChange = (dottedName, value) =>
+		setSituation({ ...situation, [dottedName]: value })
+
+	return (
+		<div css="margin: 1rem 0; padding: .6rem;  background: var(--lighterColor)">
+			<ul>
+				{demoDottedNames.map((el) => (
+					<li key={el}>
+						<label>
+							{el}{' '}
+							<input
+								type="number"
+								value={situation[el]}
+								onChange={(e) =>
+									onChange(el, e.target.value === '' ? null : e.target.value)
+								}
+							/>
+							&nbsp;{rules[el].unité}
+						</label>
+					</li>
+				))}
+			</ul>
+
+			<div>
+				<HumanWeight nodeValue={indicators.bilan || 9} />
+				<small css="margin: 0 auto; display: block; text-align: center">
+					Dont {Math.round(indicators['transport . empreinte'])} kgCO2e de
+					transport et {Math.round(indicators.logement)} kgCO2e de logement.
+				</small>
 			</div>
-		</WithEngine>
+			{/* TODO would be cool, but doesn't work with this new aside Engine 
+			<InlineCategoryChart givenEngine={engine} />
+			*/}
+		</div>
 	)
 }
