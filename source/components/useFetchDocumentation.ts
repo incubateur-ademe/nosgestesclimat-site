445a877b3b76dbaeef5ae021c7a2b9d05264d929
Clement AUGER <clem.auger@hotmail.fr>
2023-01-11 19:33:47 +0100

    changement dossier guides

--- source/components/useFetchDocumentation.ts
+++ source/components/useFetchDocumentation.ts
@@ -9,7 +9,7 @@ export default () => {
 		if (!branchData.loaded) return
 		if (NODE_ENV === 'development' && branchData.shouldUseLocalFiles) {
 			const req = require.context(
-				'raw-loader!../../nosgestesclimat/documentation/',
+				'raw-loader!../../nosgestesclimat/contenu-ecrit/',
 				true,
 				/\.(md)$/
 			)
