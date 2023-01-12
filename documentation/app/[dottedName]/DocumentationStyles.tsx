036b577fe311dbb86bb52d9f9ad7446f1feb45d9
Mael <mael.thomas@data.gouv.fr>
2023-01-12 10:45:15 +0100

    1er affichage de la doc, mais en mode 'use client'
    Donc pas ce qu'on veut. Il y a une erreur en mode server

---
+++ documentation/app/[dottedName]/DocumentationStyles.tsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const DocumentationStyle = styled.div`
+	padding: 0 0.6rem;
+	#documentationRuleRoot > p:first-of-type {
+		display: inline-block;
+		background: var(--lighterColor);
+		padding: 0.4rem 0.6rem 0.2rem;
+	}
+	header {
+		color: var(--textColor);
+		a {
+			color: var(--textColor);
+		}
+		a:hover {
+			background: var(--darkerColor) !important;
+			color: white !important;
+		}
+		h1 {
+			margin-top: 0.6rem;
+			margin-bottom: 0.6rem;
+			a {
+				text-decoration: none;
+			}
+		}
+		background: linear-gradient(60deg, var(--darkColor) 0%, var(--color) 100%);
+		padding: 0.6rem 1rem;
+		box-shadow: 0 1px 3px rgba(var(--rgbColor), 0.12),
+			0 1px 2px rgba(var(--rgbColor), 0.24);
+		border-radius: 0.4rem;
+	}
+	button {
+		color: inherit;
+	}
+	span {
+		background: inherit;
+	}
+	small {
+		background: none !important;
+	}
+	li {
+		&.active .content {
+			background-color: transparent !important;
+			a:hover {
+				color: white !important;
+			}
+		}
+	}
+`
