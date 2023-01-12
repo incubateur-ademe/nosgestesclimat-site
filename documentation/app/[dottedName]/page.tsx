c9891c5ec8b5afceba6387e52955ec6125cbc2b6
Mael <mael.thomas@data.gouv.fr>
2023-01-12 11:38:17 +0100

    Test du déploiement des pages statiques sans le moteur

--- documentation/app/[dottedName]/page.tsx
+++ documentation/app/[dottedName]/page.tsx
@@ -1,47 +1,7 @@
-'use client'
-import Link from 'next/link'
-import Engine from 'publicodes'
-import { RulePage } from 'publicodes-react'
-import { DocumentationStyle } from './DocumentationStyles'
-
-const engine = new Engine({ a: 2, b: 'a + 10', 'b . c': 'a * b', d: 'a * b' })
-
 export default function Page({ params: { dottedName } }) {
 	return (
 		<div>
 			<h1>Ma page de doc pour {dottedName} </h1>
-			<DocPage dottedName={dottedName} />
 		</div>
 	)
 }
-
-const DocPage = ({ dottedName }) => {
-	console.log('engineParsedRules:', engine.context.parsedRules)
-
-	return (
-		<DocumentationStyle>
-			<RulePage
-				language={'fr'}
-				rulePath={dottedName}
-				engine={engine}
-				documentationPath={'/documentation'}
-				renderers={{
-					Head: () => <title>Le titre de {dottedName}</title>,
-					Link: ({ to }) => <Link href={to} />,
-					Text: ({ children }) => <>Le corps de {dottedName}</>,
-				}}
-			/>
-		</DocumentationStyle>
-	)
-}
-
-// Not integratable yet, see https://github.com/betagouv/publicodes/issues/336
-const GithubContributionLink = ({ dottedName }) => (
-	<a
-		href={`https://github.com/search?q=${encodeURIComponent(
-			`repo:datagir/nosgestesclimat "${dottedName}:"`
-		)} path:data&type=code`}
-	>
-		✏️ Contribuer
-	</a>
-)
--- documentation/app/page.tsx
+++ documentation/app/page.tsx
@@ -1,10 +1,12 @@
-// Import your Client Component
+import Link from 'next/link'
 
 export default async function Page() {
-	// Forward fetched data to your Client Component
 	return (
 		<div>
 			<h1>Bienvenue sur la super doc NGC</h1>
+			Vous pouvez consulter en pages statiques <Link href="/aa">
+				aa
+			</Link> et <Link href="/bb">bb</Link>.
 		</div>
 	)
 }
