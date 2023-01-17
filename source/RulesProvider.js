758ac1417c2c6f498fafced09d92051a3a4d88a8
Mael <mael.thomas@data.gouv.fr>
2023-01-17 18:12:24 +0100

    Test d'une animation de chargement

--- source/RulesProvider.js
+++ source/RulesProvider.js
@@ -11,7 +11,7 @@ import {
 import useBranchData from 'Components/useBranchData'
 import Engine from 'publicodes'
 import { useEffect, useMemo } from 'react'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { addTranslationToBaseRules } from '../nosgestesclimat/scripts/i18n/addTranslationToBaseRules'
@@ -137,14 +137,75 @@ const EngineWrapper = ({ rules, children }) => {
 	)
 }
 
-export const WithEngine = ({
-	children,
-	fallback = (
-		<div>
-			<Trans>Chargement du modèle de calcul...</Trans>
+const AnimatedLoader = () => (
+	<div
+		css={`
+			margin-top: 10vh;
+			text-align: center;
+			.lds-ripple {
+				display: inline-block;
+				position: relative;
+				width: 80px;
+				height: 80px;
+			}
+			.lds-ripple div {
+				position: absolute;
+				border: 4px solid #fff;
+				opacity: 1;
+				border-radius: 50%;
+				animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+
+				background: radial-gradient(
+					#30c691 20%,
+					#538cf7 50%,
+					#9255c2 60%,
+					#ff3831 80%
+				);
+			}
+			.lds-ripple div:nth-child(2) {
+				animation-delay: -0.5s;
+			}
+
+			@keyframes lds-ripple {
+				0% {
+					top: 36px;
+					left: 36px;
+					width: 0;
+					height: 0;
+					opacity: 0;
+				}
+				4.9% {
+					top: 36px;
+					left: 36px;
+					width: 0;
+					height: 0;
+					opacity: 0;
+				}
+				5% {
+					top: 36px;
+					left: 36px;
+					width: 0;
+					height: 0;
+					opacity: 1;
+				}
+				100% {
+					top: 0px;
+					left: 0px;
+					width: 72px;
+					height: 72px;
+					opacity: 0;
+				}
+			}
+		`}
+	>
+		<div className="lds-ripple">
+			<div></div>
+			<div></div>
 		</div>
-	),
-}) => {
+	</div>
+)
+
+export const WithEngine = ({ children, fallback = <AnimatedLoader /> }) => {
 	const dispatch = useDispatch()
 	const engineState = useSelector((state) => state.engineState)
 
