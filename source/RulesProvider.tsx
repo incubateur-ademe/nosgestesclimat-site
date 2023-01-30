04b74ab7acf2e85c15a32f098f34c3e43626da63
Mael <mael.thomas@data.gouv.fr>
2023-01-30 17:08:25 +0100

    On évite un state de chargement sur le composant WithEngine de la conversation
    Quand on passe au chapitre services sociétaux qui nécessitent les règles
    complètes

--- source/RulesProvider.tsx
+++ source/RulesProvider.tsx
@@ -90,8 +90,8 @@ export const WithEngine = ({
 	useRules(options)
 
 	useEffect(() => {
-		if (engineState && currentRulesOptions?.optimized === false) return // if the full set of rules is loaded, don't reload the optimzed set, since the former includes the latter
-		dispatch({ type: 'SET_ENGINE', to: 'requested' })
+		// We don't need to set `resquested` again in case of a change of options. The useRules hooks observe the options param
+		if (engineState == null) dispatch({ type: 'SET_ENGINE', to: 'requested' })
 		return
 	}, [])
 
