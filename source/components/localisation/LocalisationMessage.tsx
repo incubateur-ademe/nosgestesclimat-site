1ee886601b57fe56c79da5b780e0b8bca5c1eb83
Clement AUGER <clem.auger@hotmail.fr>
2023-02-08 20:18:53 +0100

    new localisation message for non supported countries

--- source/components/localisation/LocalisationMessage.tsx
+++ source/components/localisation/LocalisationMessage.tsx
@@ -1,8 +1,11 @@
 import { Trans } from 'react-i18next'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import IllustratedMessage from '../ui/IllustratedMessage'
 import { usePersistingState } from '../utils/persistState'
 import useLocalisation, {
+	getCountryNameInFrench,
+	getFlagImgSrc,
 	getSupportedFlag,
 	isSupportedRegion,
 } from './useLocalisation'
@@ -13,17 +16,83 @@ export default () => {
 		[]
 	)
 	const localisation = useLocalisation()
-	if (!localisation?.country) return null
+	const currentLang = useSelector((state) => state.currentLang)
+
+	if (!localisation?.country) return
+	if (messagesRead.includes(localisation?.country.code)) return
+
 	const supported = isSupportedRegion(localisation?.country?.code)
-	if (!supported) return null
-	const { code, gentilé, nom } = supported
-	if (code === 'FR') return null
-	if (messagesRead.includes(code)) return null
+
+	if (supported && localisation?.country?.code === 'FR') return
+
+	const { code, gentilé, nom } = supported || isSupportedRegion('FR')
+
 	const flag = getSupportedFlag(localisation?.country.code)
+	const countryName =
+		currentLang == 'Fr'
+			? getCountryNameInFrench(localisation?.country?.code)
+			: localisation?.country?.name
 
 	const versionName = gentilé ?? nom
 
-	return (
+	return !supported ? (
+		<IllustratedMessage
+			width="32rem"
+			direction="row"
+			backgroundcolor="#fff8d3"
+			message={
+				<div>
+					<p>
+						<Trans>
+							Nous avons détecté que vous faites cette simulation depuis
+						</Trans>{' '}
+						{countryName}
+						<img
+							src={
+								getSupportedFlag(localisation?.country?.code) ||
+								getFlagImgSrc(localisation?.country?.code)
+							}
+							aria-hidden="true"
+							css={`
+								height: 1rem;
+								margin: 0 0.3rem;
+								vertical-align: sub;
+							`}
+						/>
+						.
+						<p css="margin-top: 0.5rem">
+							<b>
+								<Trans i18nKey="components.localisation.LocalisationMessage.warnMessage">
+									Votre région n'est pas encore supportée, vous utilisez donc le
+									modèle Français par défault.
+								</Trans>
+							</b>
+						</p>
+					</p>
+					<p>
+						<small>
+							<Link to="/profil">
+								<Trans>Choisissez une région parmi celles disponibles !</Trans>
+							</Link>
+						</small>
+					</p>
+					<button
+						className="ui__ button plain small "
+						css={`
+							margin-left: auto;
+							margin-right: 0rem;
+							display: block !important;
+						`}
+						onClick={() =>
+							setRead([...messagesRead, localisation?.country?.code])
+						}
+					>
+						<Trans>J'ai compris</Trans>
+					</button>
+				</div>
+			}
+		/>
+	) : (
 		<IllustratedMessage
 			width="32rem"
 			direction="row"
