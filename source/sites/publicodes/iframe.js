6e9fa8f2b14325843e6177d4d4b7705a2ac2aa1a
Mael <mael.thomas@data.gouv.fr>
2023-02-22 11:43:12 +0100

    Ajout d'un bouton pour passer en mode plein écran sur l'iframe

--- source/sites/publicodes/iframe.js
+++ source/sites/publicodes/iframe.js
@@ -28,8 +28,11 @@ const iframeAttributes = {
 	allowfullscreen: true,
 	webkitallowfullscreen: true,
 	mozallowfullscreen: true,
+	allow: 'fullscreen',
 	id: 'iframeNGC',
 }
+
+const color = '#32337b'
 document.head.insertAdjacentHTML(
 	'beforeend',
 	`<style>
@@ -45,9 +48,21 @@ document.head.insertAdjacentHTML(
 	@media (min-width: 800px){
 	  #iframeNGC{
 	  max-width: 450px; /* On large monitors, the iframe should not extend too much, as to avoid confusion between the iframe (viewed as an app) and the rest of the content, e.g. when it's integrated in a blog article. Remember : the website is designed mobile first, so should work perfectly on this width */
-		border: 8px solid #32337b;
+		border: 8px solid ${color};
 		}
     }
+	#iframeNGC.fullscreen {
+
+    width: 100%;
+    height: 100%;
+	max-height: 100%;
+	max-width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+	background: white;
+
+	}
     </style>`
 )
 
@@ -65,7 +80,28 @@ margin: 1rem auto .6rem;
 text-align: center
 
 `
+const fullscreenButton = document.createElement('button')
+fullscreenButton.innerHTML = `
+<img width="14px" height="14px" src="https://nosgestesclimat.fr/images/fullscreen.svg" style="filter: invert(1); vertical-align: middle; cursor: pointer"/>
+Passer en mode plein écran
+
+`
+fullscreenButton.style.cssText = `
+display: block; margin: 0 auto;
+border: none;
+padding: .2rem .8rem;
+border-radius: .4rem;
+background: ${color};
+font-weight: bold;
+color: white;
+
+`
+fullscreenButton.addEventListener('click', (event) => {
+	document.querySelector('#iframeNGC').classList.add('fullscreen')
+})
 
 script.parentNode.insertBefore(link, script)
+// script.parentNode.insertBefore(fullscreenButton, script)
+// TODO : works, but we need to let the user come back !
 
 script.parentNode.insertBefore(iframe, script)
