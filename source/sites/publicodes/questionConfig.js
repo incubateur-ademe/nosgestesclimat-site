e05ff2b67fc790530fcc487fa8639c13b685c094
Clement AUGER <clem.auger@hotmail.fr>
2023-03-02 18:31:29 +0100

    question 'divers . autres produits' non prioritaire

--- source/sites/publicodes/questionConfig.js
+++ source/sites/publicodes/questionConfig.js
@@ -1,5 +1,5 @@
 export const questionConfig = {
-	'non prioritaires': [],
+	'non prioritaires': ['divers . autres produits'],
 	'liste prioritaire': ['alimentation . boisson . chaude'],
 	'liste blanche': [],
 	'liste noire': ['transport . voiture . aide km'],
