41b615a584c48907c5e100b31787ed81083ab588
Mael <laem@kont.me>
2023-03-14 10:55:18 +0100

    Ajout d'un lien vers la future page /questions

--- source/sites/publicodes/ModelStatsBlock.tsx
+++ source/sites/publicodes/ModelStatsBlock.tsx
@@ -1,5 +1,6 @@
 import { Trans } from 'react-i18next'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Loading = () => <p>Chargement du modèle...</p>
 export default () => {
@@ -13,12 +14,23 @@ export default () => {
 	const NumberOfRules = () => <span>{numberOfRules}</span>
 	const NumberOfQuestions = () => <span>{numberOfQuestions}</span>
 	return (
-		<p>
-			<Trans i18nKey={'model.stats'}>
-				Le modèle comprend aujourd'hui <NumberOfRules /> règles de calcul. Parmi
-				elles, <NumberOfQuestions /> règles sont des questions à poser à
-				l'utilisateur pour calculer un résultat précis.
-			</Trans>
-		</p>
+		<div>
+			<p>
+				<Trans i18nKey={'model.stats'}>
+					Le modèle comprend aujourd'hui <NumberOfRules /> règles de calcul.
+					Parmi elles, <NumberOfQuestions /> règles sont des questions à poser à
+					l'utilisateur pour calculer un résultat précis.
+				</Trans>
+			</p>
+			<p>
+				<Trans i18nKey={'model.questions'}>
+					Découvrez{' '}
+					<Link to="/questions">
+						la liste des questions disponibles dans le modèle
+					</Link>
+					.
+				</Trans>
+			</p>
+		</div>
 	)
 }
