9688941b94b9fb5d2f80e45aea138686ad5e2be5
Clement AUGER <clem.auger@hotmail.fr>
2023-03-20 16:39:41 +0100

    banner if not detected

--- source/components/localisation/LocalisationMessage.tsx
+++ source/components/localisation/LocalisationMessage.tsx
@@ -20,7 +20,6 @@ export default () => {
 	const regionParams = supportedRegion(localisation?.country?.code)
 	const flag = getFlag(localisation?.country?.code)
 
-	if (!localisation?.country) return
 	if (messagesRead.includes(localisation?.country?.code)) return
 
 	if (localisation?.country?.code === defaultModel) return
@@ -35,43 +34,108 @@ export default () => {
 			? regionParams?.gentilé ?? regionParams?.nom
 			: localisation?.country?.name
 
-	return !regionParams ? (
+	return (
 		<IllustratedMessage
 			width="32rem"
 			direction="row"
 			backgroundcolor="#fff8d3"
 			message={
 				<div>
-					<p>
-						<Trans>
-							Nous avons détecté que vous faites cette simulation depuis
-						</Trans>{' '}
-						{countryName}
-						<img
-							src={flag ?? getFlagImgSrc(localisation?.country?.code)}
-							aria-hidden="true"
-							css={`
-								height: 1rem;
-								margin: 0 0.3rem;
-								vertical-align: sub;
-							`}
-						/>
-						.
-						<p css="margin-top: 0.5rem">
-							<b>
-								<Trans i18nKey="components.localisation.LocalisationMessage.warnMessage">
-									Votre région n'est pas encore supportée, le modèle Français
-									vous est proposé par défaut.
-								</Trans>
-							</b>
+					{regionParams ? (
+						<p>
+							<Trans
+								i18nKey={'components.localisation.LocalisationMessage.version'}
+							>
+								Vous utilisez la version <strong>{{ versionName }}</strong> du
+								test.
+							</Trans>
+							<img
+								src={flag}
+								aria-hidden="true"
+								css={`
+									height: 1rem;
+									margin: 0 0.3rem;
+									vertical-align: sub;
+								`}
+							/>
+							{regionParams?.code !== defaultModel && (
+								<span>
+									{' '}
+									<Trans i18nKey="components.localisation.LocalisationMessage.betaMsg">
+										Elle est actuellement en version <strong>bêta</strong>.
+									</Trans>
+								</span>
+							)}{' '}
 						</p>
-					</p>
+					) : localisation ? (
+						<p>
+							<Trans>
+								Nous avons détecté que vous faites cette simulation depuis
+							</Trans>{' '}
+							{countryName}
+							<img
+								src={flag ?? getFlagImgSrc(localisation?.country?.code)}
+								aria-hidden="true"
+								css={`
+									height: 1rem;
+									margin: 0 0.3rem;
+									vertical-align: sub;
+								`}
+							/>
+							.
+							<p css="margin-top: 0.5rem">
+								<b>
+									<Trans i18nKey="components.localisation.LocalisationMessage.warnMessage">
+										Votre région n'est pas encore supportée, le modèle Français
+										vous est proposé par défaut.
+									</Trans>
+								</b>
+							</p>
+							<img
+								src={getFlagImgSrc(defaultModel)}
+								aria-hidden="true"
+								css={`
+									height: 1rem;
+									margin: 0 0.3rem;
+									vertical-align: sub;
+								`}
+							/>
+						</p>
+					) : (
+						<p>
+							<Trans i18nKey="components.localisation.LocalisationMessage.warnMessage2">
+								Nous n'avons pas pu détecter votre pays de simulation, le modèle
+								Français vous est proposé par défaut.
+							</Trans>
+							<img
+								src={getFlagImgSrc(defaultModel)}
+								aria-hidden="true"
+								css={`
+									height: 1rem;
+									margin: 0 0.3rem;
+									vertical-align: sub;
+								`}
+							/>
+						</p>
+					)}
 					<p>
-						<small>
-							<Link to="/profil">
-								<Trans>Choisissez une région parmi celles disponibles !</Trans>
-							</Link>
-						</small>
+						{localisation && regionParams ? (
+							<small>
+								<Trans>Pas votre région ?</Trans>{' '}
+								<Link to="/profil">
+									<Trans>Choisissez la vôtre</Trans>
+								</Link>
+								.
+							</small>
+						) : (
+							<small>
+								<Link to="/profil">
+									<Trans>
+										Choisissez une région parmi celles disponibles !
+									</Trans>
+								</Link>
+							</small>
+						)}
 					</p>
 					<button
 						className="ui__ button plain small "
@@ -84,58 +148,6 @@ export default () => {
 							dispatch({
 								type: 'SET_LOCALISATION_BANNERS_READ',
 								regions: [...messagesRead, localisation?.country?.code],
-							})
-						}
-					>
-						<Trans>J'ai compris</Trans>
-					</button>
-				</div>
-			}
-		/>
-	) : (
-		<IllustratedMessage
-			width="32rem"
-			direction="row"
-			backgroundcolor="#fff8d3"
-			image={flag}
-			message={
-				<div>
-					<p>
-						<Trans
-							i18nKey={'components.localisation.LocalisationMessage.version'}
-						>
-							Vous utilisez la version <strong>{{ versionName }}</strong> du
-							test.
-						</Trans>
-						{regionParams?.code !== defaultModel && (
-							<span>
-								{' '}
-								<Trans i18nKey="components.localisation.LocalisationMessage.betaMsg">
-									Elle est actuellement en version <strong>bêta</strong>.
-								</Trans>
-							</span>
-						)}{' '}
-					</p>
-					<p>
-						<small>
-							<Trans>Pas votre région ?</Trans>{' '}
-							<Link to="/profil">
-								<Trans>Choisissez la vôtre</Trans>
-							</Link>
-							.
-						</small>
-					</p>
-					<button
-						className="ui__ button plain small "
-						css={`
-							margin-left: auto;
-							margin-right: 0rem;
-							display: block !important;
-						`}
-						onClick={() =>
-							dispatch({
-								type: 'SET_LOCALISATION_BANNERS_READ',
-								regions: [...messagesRead, regionParams?.code],
 							})
 						}
 					>
