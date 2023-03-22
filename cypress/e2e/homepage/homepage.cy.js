65ee7aeefc9c645f6a2c59ef81388919201cb6da
Florian <hi@florianpanchout.com>
2023-03-22 15:12:12 +0100

    test: add first e2e tests

---
+++ cypress/e2e/homepage/homepage.cy.js
@@ -0,0 +1,12 @@
+describe('check for homepage status', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:8082')
+	})
+
+	it('has a start button', () => {
+		cy.contains('Take the test').should('be.visible')
+	})
+	it('has a group button', () => {
+		cy.contains('As a group').should('be.visible')
+	})
+})
