423f44cd88116ce18bdd3bddc55558ccd5650c84
Mael <laem@kont.me>
2023-03-31 13:21:29 +0200

    Accessibilité Printemps pour la planète

--- source/components/PartnerBanner.tsx
+++ source/components/PartnerBanner.tsx
@@ -7,9 +7,11 @@ export default () => (
 	>
 		<a
 			href="https://www.jeveuxaider.gouv.fr/engagement/printemps-pour-la-planete/"
+			title="Découvrez les actoins de la campagne Printemps pour la planète"
 			target="_blank"
 		>
 			<img
+				alt="Le logo vert et bleu de la campagne Printemps pour la planète "
 				src="/images/Printemps-pour-la-planete.svg"
 				width="100"
 				height="100"
