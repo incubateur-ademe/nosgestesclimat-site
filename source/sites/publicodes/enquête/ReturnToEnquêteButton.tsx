1fcc47edde0f6921ee9c96db87b5c00346dc4ff8
Mael <laem@kont.me>
2023-04-03 15:58:53 +0200

    Désactivation du lien pour revenir à l'enquête si test pas terminé

--- source/sites/publicodes/enquête/ReturnToEnquêteButton.tsx
+++ source/sites/publicodes/enquête/ReturnToEnquêteButton.tsx
@@ -1,15 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useTestCompleted } from '../../../selectors/simulationSelectors'
 
 export default () => {
 	const dispatch = useDispatch()
 	const enquête = useSelector((state) => state.enquête)
+	const testCompleted = useTestCompleted()
 	if (!enquête) return null
 	const id = enquête.userID
 	const url = `https://ow3.cawi.fr/xcawi2-testlienext/${id}`
 	return (
-		<a href={url} target="_blank">
+		<a
+			href={url}
+			target="_blank"
+			css={!testCompleted ? 'pointer-events: none;' : ''}
+		>
 			<button
-				className="ui__ button "
+				className={'ui__ button ' + (!testCompleted ? 'disabled' : '')}
 				onClick={() => dispatch({ type: 'QUIT_ENQUÊTE' })}
 			>
 				✅ Revenir à l'enquête
