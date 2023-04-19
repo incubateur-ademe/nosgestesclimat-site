ad87ddd911f2f98256174e93ec8b1900a9d28410
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-04-19 12:23:58 +0200

    Appelle la fonction inFrame dans la condition

--- source/Tracker.ts
+++ source/Tracker.ts
@@ -37,7 +37,7 @@ export default class Tracker {
 			// https://gist.github.com/iansltx/18caf551baaa60b79206. We could probably
 			// do better but for now we don't track action of iOs Safari user in
 			// iFrame -- to avoid errors in the number of visitors in our stats.
-			if (iOSSafari && inIframe) return
+			if (iOSSafari && inIframe()) return
 
 			// Could be due to an adblocker not allowing the script to set this global attribute
 			if (!window.plausible) return
