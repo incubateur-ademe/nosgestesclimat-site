3f816a2a3fe893f5744f65eb97e23c16b2e8da69
Emile Rolley <emile.rolley@tuta.io>
2023-04-20 12:35:10 +0200

    ci(e2e): setup Cypress cloud env

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+	projectId: 'dbxhpr',
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
