498e214582f33b6598749cd00b33b94fdeea1e2e
Emile Rolley <emile.rolley@tuta.io>
2023-05-24 13:29:22 +0200

    refactor(types): fixes all types errors in CategoryVisualisation.tsx

--- source/sites/publicodes/CategoryVisualisation.tsx
+++ source/sites/publicodes/CategoryVisualisation.tsx
@@ -1,20 +1,24 @@
-import { CategoryLabel } from 'Components/conversation/UI'
+import { CategoryLabel } from '@/components/conversation/UI'
+import { getSubcategories } from '@/components/publicodesUtils'
+import AnimatedTargetValue from '@/components/ui/AnimatedTargetValue'
+import { useEngine } from '@/components/utils/EngineContext'
+import { AppState } from '@/reducers/rootReducer'
 import emoji from 'react-easy-emoji'
 import { useSelector } from 'react-redux'
-import { getSubcategories } from '../../components/publicodesUtils'
-import AnimatedTargetValue from '../../components/ui/AnimatedTargetValue'
-import { useEngine } from '../../components/utils/EngineContext'
 import SubCategoriesChart from './chart/SubCategoriesChart'
 
 export default ({ questionCategory: category, hideMeta = false }) => {
-	const rules = useSelector((state) => state.rules)
+	const rules = useSelector((state: AppState) => state.rules)
 	const engine = useEngine()
 
 	// The aim of this component is to visualize sums. Sometimes, relevant sums are hidden behind a division
 	// it should be visualized elsewhere
 	const subCategories = getSubcategories(rules, category, engine)
 
-	const categoryValue = Math.round(engine.evaluate(category.name).nodeValue)
+	const categoryValue = Math.round(
+		// NOTE(@EmileRolley): we assume that the category is a numeric value
+		engine.evaluate(category.name).nodeValue as number
+	)
 
 	return (
 		<div
