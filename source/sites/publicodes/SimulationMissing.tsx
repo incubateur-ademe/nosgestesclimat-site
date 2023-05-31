8c0b4229ec09e637404ac2663f57c0b5ea2371b0
Mael <laem@kont.me>
2023-05-31 19:14:19 +0200

    Simplification de l'avertissement débloquage parcours action

--- source/sites/publicodes/SimulationMissing.tsx
+++ source/sites/publicodes/SimulationMissing.tsx
@@ -1,34 +1,38 @@
 import { Trans } from 'react-i18next'
 import { Link } from 'react-router-dom'
-import IllustratedMessage from '../../components/ui/IllustratedMessage'
 
 export default ({}) => {
 	return (
 		<div
 			className="ui__ card light colored content"
-			css="margin: 1.6em 0.5rem 1.6rem 0.5rem"
+			css={`
+				margin: 1.6em 0.5rem 1.6rem 0.5rem;
+				text-align: center;
+				padding: 1rem !important;
+			`}
 		>
-			<IllustratedMessage
-				inline
-				emoji="🔒"
-				message={
-					<p>
-						<Trans i18nKey={'publicodes.SimulationMissing.simulationManquante'}>
-							Pour débloquer ce parcours, vous devez d'abord terminer le test.
-						</Trans>
-					</p>
-				}
-			/>
-			<div css="margin: 2rem auto 1rem; text-align: center">
+			<p
+				css={`
+					text-align: center;
+				`}
+			>
+				🔒{' '}
+				<Trans i18nKey={'publicodes.SimulationMissing.simulationManquante'}>
+					Pour débloquer ce parcours, vous devez d'abord terminer le test.
+				</Trans>
+			</p>
+			<div css="margin: 1rem auto; text-align: center">
 				<Link to="/simulateur/bilan" className="ui__ plain button">
 					<Trans>Faire le test</Trans>
 				</Link>
 			</div>
-			<p css="text-align: center; max-width: 100%; margin: 0 auto;">
-				<Trans i18nKey={'publicodes.SimulationMissing.personnas'}>
-					💡 Vous pouvez aussi continuer avec un{' '}
-					<Link to="/personas">profil type</Link>.
-				</Trans>
+			<p css="text-align: center; margin: 0 ">
+				<small>
+					<Trans i18nKey={'publicodes.SimulationMissing.personnas'}>
+						Vous pouvez aussi continuer avec un{' '}
+						<Link to="/personas">profil type</Link>.
+					</Trans>
+				</small>
 			</p>
 		</div>
 	)
