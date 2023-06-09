1251aa0c6b503ababaf8e008708ace4a426ddac7
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-06-09 10:45:00 +0200

    feat: Start saving form info

--- source/pages/groupe-amis/components/TextInputGroup.tsx
+++ source/pages/groupe-amis/components/TextInputGroup.tsx
@@ -7,6 +7,8 @@ type Props = {
 	helperText?: string
 	className?: string
 	placeholder?: string
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+	value?: string
 }
 
 export default function TextInputGroup({
@@ -17,9 +19,11 @@ export default function TextInputGroup({
 	helperText,
 	className,
 	placeholder,
+	onChange,
+	value,
 }: Props) {
 	return (
-		<div className={`flex flex-col ${className}`}>
+		<div className={`flex flex-col ${className}`} aria-live="polite">
 			<label htmlFor={name}>
 				<span
 					className={`text-sm font-bold text-slate-900 ${
@@ -39,8 +43,15 @@ export default function TextInputGroup({
 				className={`border border-slate-200 rounded-sm px-3 py-2 mt-3 focus:ring-2 focus:ring-pink-700 focus:border-transparent ${
 					error ? 'ring-2 ring-red-700 border-red-700' : ''
 				}}`}
+				onChange={onChange}
+				aria-describedby={`error-${name}`}
+				value={value}
 			/>
-			{error && <span className="text-sm text-red-700 mt-1">{error}</span>}
+			{error && (
+				<span id={`error-${name}`} className="text-sm text-red-700 mt-1">
+					{error}
+				</span>
+			)}
 		</div>
 	)
 }
