998afa3a73858d738e7ff43a2808673fb7956174
Emile Rolley <emile.rolley@tuta.io>
2023-06-13 10:20:17 +0200

    refactor(types): remove errors and warnings from Simulateur.tsx

--- source/reducers/rootReducer.ts
+++ source/reducers/rootReducer.ts
@@ -42,16 +42,29 @@ type QuestionsKind =
 	| 'liste'
 	| 'liste noire'
 
+export type ObjectifsConfig =
+	| Array<DottedName>
+	| Array<{ icône: string; nom: string; objectifs: Array<DottedName> }>
+
 export type SimulationConfig = {
-	objectifs:
-		| Array<DottedName>
-		| Array<{ icône: string; nom: string; objectifs: Array<DottedName> }>
+	objectifs: ObjectifsConfig
 	'objectifs cachés': Array<DottedName>
 	situation: Simulation['situation']
 	bloquant?: Array<DottedName>
 	questions?: Partial<Record<QuestionsKind, Array<DottedName>>>
 	branches?: Array<{ nom: string; situation: SimulationConfig['situation'] }>
 	'unité par défaut': string
+}
+
+export function objectifsConfigToDottedNameArray(
+	obj: ObjectifsConfig
+): Array<DottedName> {
+	if (obj.length === 0) {
+		return []
+	} else if (typeof obj[0] === 'object') {
+		return obj.flatMap((v) => v.objectifs)
+	}
+	return obj
 }
 
 type Situation = Partial<Record<DottedName, any>>
