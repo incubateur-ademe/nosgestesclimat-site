7253d28f937dbbf50317a0979be3249437ea6540
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-07-05 18:25:22 +0200

    feat: Ajoute les noms de groupes +

--- source/pages/mes-groupes/components/GroupItem.tsx
+++ source/pages/mes-groupes/components/GroupItem.tsx
@@ -35,11 +35,11 @@ const EMOJIS = [
 export default function GroupItem({ group, index }: Props) {
 	return (
 		<Link
-			to={`/groupe/${group?._id}`}
+			to={`/groupes/resultats?groupId=${group?._id}`}
 			className="border-solid border-[1px] border-gray-200 bg-gray-100 no-underline rounded-sm px-5 py-2 decoration-auto mb-3"
 		>
 			<div className="flex items-center justify-between py-4">
-				<div className="flex items-center">
+				<div className="flex items-center w-full">
 					<div className="flex-shrink-0 text-2xl">
 						<span>{EMOJIS[index]}</span>
 					</div>
@@ -47,9 +47,17 @@ export default function GroupItem({ group, index }: Props) {
 						<div className="text-md font-bold text-gray-900">
 							{group?.name ?? 'Nom du groupe'}
 						</div>
-						<div className="text-sm text-violet-900">
-							{group?.members?.length ?? 0} <Trans>participant</Trans>
-							{group?.members?.length > 1 ? 's' : ''}
+						<div className="text-sm text-violet-900 flex gap-1 max-w-max">
+							<span className="whitespace-nowrap">
+								{group?.members?.length ?? 0} <Trans>participant</Trans>
+								{group?.members?.length > 1 ? 's' : ''}
+							</span>{' '}
+							<span> - </span>{' '}
+							<div className="text-ellipsis overflow-hidden whitespace-nowrap w-12 md:w-2/5">
+								Roger, Adrien, Tugdual, Jean, Jeanne, Jean-Pierre, Jean-Paul,
+								Sartre, rougel, Adrien, Tugdual, Jean, Jeanne, Jean-Pierre,
+								Jean-Paul, Sartre, rougel
+							</div>
 						</div>
 					</div>
 				</div>
