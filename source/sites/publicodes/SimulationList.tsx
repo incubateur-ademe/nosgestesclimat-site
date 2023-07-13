3d8690a0500d7f56a171504e9031c9b39c4fda4d
Clement AUGER <clem.auger@hotmail.fr>
2023-07-13 11:04:36 +0200

    fix: css simulation list

--- source/sites/publicodes/SimulationList.tsx
+++ source/sites/publicodes/SimulationList.tsx
@@ -15,8 +15,60 @@ export default ({ dispatch, list, currentSimulationId }) => {
 					simulation.date !== undefined ? new Date(simulation.date) : new Date()
 				return (
 					<li key={simulation.id} css="list-style-type: none">
-						<details css="display: inline-block;">
-							<summary>{dateSimu.toLocaleDateString()}</summary>
+						<details>
+							<summary>
+								<div
+									css={`
+										display: inline-flex;
+									`}
+								>
+									<span>{dateSimu.toLocaleDateString()}</span>
+									<span
+										css={`
+											margin-left: 0.25rem;
+											width: 8rem;
+											overflow: hidden;
+											white-space: nowrap;
+											text-overflow: ellipsis;
+											@media (max-width: 800px) {
+												display: none;
+											}
+										`}
+									>
+										- {simulation.id}
+									</span>
+									{currentSimulationId === simulation.id ? (
+										<span css="margin: 0 1rem">
+											✅ <Trans>Chargée</Trans>
+										</span>
+									) : (
+										<span>
+											<button
+												className={'ui__ button simple small'}
+												css="margin: 0 1rem"
+												onClick={() => {
+													dispatch(setCurrentSimulation(simulation))
+													dispatch(setActionsChoices(simulation.actionChoices))
+													dispatch(
+														setAllStoredTrajets(simulation.storedTrajets)
+													)
+												}}
+											>
+												<Trans>Charger</Trans>
+											</button>
+											<button
+												className={'ui__ button simple small'}
+												css="margin: 0 1rem"
+												onClick={() => {
+													dispatch(deleteSimulationById(simulation.id))
+												}}
+											>
+												<Trans>supprimer</Trans>
+											</button>
+										</span>
+									)}
+								</div>
+							</summary>
 							<ul>
 								<li>
 									Date complète : {dateSimu.toLocaleDateString()}{' '}
@@ -25,34 +77,6 @@ export default ({ dispatch, list, currentSimulationId }) => {
 								<li>Identifiant : {simulation.id}.</li>
 							</ul>
 						</details>
-						{currentSimulationId === simulation.id ? (
-							<span css="margin: 0 1rem">
-								✅ <Trans>Chargée</Trans>
-							</span>
-						) : (
-							<span>
-								<button
-									className={'ui__ button simple small'}
-									css="margin: 0 1rem"
-									onClick={() => {
-										dispatch(setCurrentSimulation(simulation))
-										dispatch(setActionsChoices(simulation.actionChoices))
-										dispatch(setAllStoredTrajets(simulation.storedTrajets))
-									}}
-								>
-									<Trans>Charger</Trans>
-								</button>
-								<button
-									className={'ui__ button simple small'}
-									css="margin: 0 1rem"
-									onClick={() => {
-										dispatch(deleteSimulationById(simulation.id))
-									}}
-								>
-									<Trans>supprimer</Trans>
-								</button>
-							</span>
-						)}
 					</li>
 				)
 			})}
