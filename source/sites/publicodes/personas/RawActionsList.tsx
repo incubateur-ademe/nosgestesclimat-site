ae9cafff5f29a27d5906f9de1f43caf3ab76b415
Clement AUGER <clem.auger@hotmail.fr>
2023-07-13 16:09:23 +0200

    feat: liste actions par catégorie + type

--- source/sites/publicodes/personas/RawActionsList.tsx
+++ source/sites/publicodes/personas/RawActionsList.tsx
@@ -1,3 +1,9 @@
+import {
+	Category,
+	DottedName,
+	extractCategoriesNamespaces,
+	splitName,
+} from '@/components/publicodesUtils'
 import { useEngine } from '@/components/utils/EngineContext'
 import { AppState } from '@/reducers/rootReducer'
 import { Trans, useTranslation } from 'react-i18next'
@@ -18,15 +24,60 @@ export default () => {
 		metric: null,
 	})
 
-	const actionsList = actionResultsRaw.map((action) => {
-		const { dottedName, title } = action
+	type CustomAction = {
+		correctedValue: number
+		dottedName: DottedName
+		sign: string
+		stringValue: string
+		title: string
+		unit: string
+	}
+
+	type ActionsList = Array<CustomAction>
+
+	type ActionsListByCategory = Array<
+		Category & {
+			actions?: ActionsList
+		}
+	>
+
+	const actionsList: ActionsList = actionResultsRaw.map((actionRule) => {
+		const { dottedName, title }: { dottedName: DottedName; title: string } =
+			actionRule
 		const { correctedValue, stringValue, unit, sign } = getFormattedActionValue(
 			{ t, i18n },
 			dottedName,
 			engine
 		)
-		return { dottedName, title, correctedValue, stringValue, unit, sign }
+		return {
+			dottedName,
+			title,
+			correctedValue,
+			stringValue,
+			unit,
+			sign,
+		}
 	})
+
+	const categories = extractCategoriesNamespaces(rules, engine).reduce(
+		(obj, category) => {
+			obj[category.dottedName] = category
+			return obj
+		},
+		{} as Array<Category>
+	)
+
+	const actionsListByCategory: ActionsListByCategory = actionsList.reduce(
+		(obj, action) => {
+			const category = splitName(action.dottedName)[0]
+			obj[category].actions = {
+				...obj[category].actions,
+				[action.dottedName]: action,
+			}
+			return obj
+		},
+		categories
+	)
 
 	const numberOfActions = actionsList.length
 	const rawTotalReduction = actionsList.reduce(
@@ -61,14 +112,28 @@ export default () => {
 				</strong>
 				.
 			</p>
-			<ul>
-				{actionsList.map(({ dottedName, title, stringValue, unit, sign }) => (
-					<li key={dottedName}>
-						{dottedName} | {title} |{' '}
-						{stringValue ? `${sign} ${stringValue} ${unit}` : 'Non quantifiée'}
-					</li>
-				))}
-			</ul>
+			{Object.values(actionsListByCategory).map(
+				(category) =>
+					category?.actions && (
+						<details key={category.dottedName}>
+							<summary>
+								{category.title} ({Object.values(category.actions).length})
+							</summary>
+							<ul>
+								{Object.values(category.actions).map(
+									({ dottedName, title, stringValue, unit, sign }) => (
+										<li key={dottedName}>
+											{title} :{' '}
+											{stringValue
+												? `${sign} ${stringValue} ${unit}`
+												: 'Non quantifiée'}
+										</li>
+									)
+								)}
+							</ul>
+						</details>
+					)
+			)}
 		</div>
 	)
 }
