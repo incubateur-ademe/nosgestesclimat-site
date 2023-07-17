32bcc77b014ce2d4878edb5b8ac61f8c7ed7929e
Clement AUGER <clem.auger@hotmail.fr>
2023-07-17 11:29:42 +0200

    feat: ajout meta

--- source/sites/publicodes/pages/Budget.tsx
+++ source/sites/publicodes/pages/Budget.tsx
@@ -1,4 +1,5 @@
 import { Markdown } from '@/components/utils/markdown'
+import Meta from '@/components/utils/Meta'
 import contentFrBottom from '@/locales/pages/fr/budgetBottom.md'
 import contentFrTop from '@/locales/pages/fr/budgetTop.md'
 import { useTranslation } from 'react-i18next'
@@ -8,7 +9,10 @@ export default () => {
 	const { t } = useTranslation()
 	return (
 		<div className="ui__ container">
-			{/* <Meta title={title} description={description} image={image} /> */}
+			<Meta
+				title={t('meta.publicodes.Budget.title')}
+				description={t('meta.publicodes.Budget.description')}
+			/>
 			<Markdown children={contentFrTop} />
 			<SelectYear />
 			<Markdown children={contentFrBottom} />
