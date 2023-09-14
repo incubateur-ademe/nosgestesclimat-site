78da3fa3008976a9e474281250897f69280a346c
Clement AUGER <clem.auger@hotmail.fr>
2023-09-14 20:13:33 +0200

    add PR #1302

---
+++ source/components/IframeResizer.tsx
@@ -0,0 +1,25 @@
+import { getIsIframe } from '@/utils'
+import { useEffect } from 'react'
+
+export function IframeResizer() {
+	const isIframe = getIsIframe()
+	useEffect(() => {
+		// The code below communicate with the iframe.js script on a host site
+		// to automatically resize the iframe when its inner content height
+		// change.
+
+		if (!isIframe) {
+			return
+		}
+
+		const minHeight = 800 // Also used in iframe.js
+		const observer = new ResizeObserver(([entry]) => {
+			const value = Math.max(minHeight, entry.contentRect.height)
+			window.parent?.postMessage({ kind: 'resize-height', value }, '*')
+		})
+		observer.observe(window.document.body)
+		return () => observer.disconnect()
+	}, [isIframe])
+
+	return null
+}
