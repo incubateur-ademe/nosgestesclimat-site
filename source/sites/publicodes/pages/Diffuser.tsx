1ec65bbfbf197fd608d5169e85e4bce2b87a71fc
Emile Rolley <emile.rolley@tuta.io>
2023-10-25 17:16:21 +0200

    refactor(i18n): use 'en' instead of 'en-us'

--- source/sites/publicodes/pages/Diffuser.tsx
+++ source/sites/publicodes/pages/Diffuser.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import { Lang } from '../../../locales/translation'
 import MarkdownPage from './MarkdownPage'
 
-import contentEn from '../../../locales/pages/en-us/diffuser.md'
+import contentEn from '../../../locales/pages/en/diffuser.md'
 // import contentEs from '../../../locales/pages/es/diffuser.md'
 import contentFr from '../../../locales/pages/fr/diffuser.md'
 // import contentIt from '../../../locales/pages/it/diffuser.md'
